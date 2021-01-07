7fa8559bfe1e85820fcd7a70e904d22dbfcd85ef
Aybek Sultanov <aybek-sultanov@inbox.ru>
2021-01-08 01:39:57 +0600

    feat/add fetch request (get upvotes from db)

--- front/src/pages/ArticlePage.js
+++ front/src/pages/ArticlePage.js
@@ -1,4 +1,4 @@
-import { React } from "react";
+import React, { useState, useEffect } from "react";
 import ArticleList from '../componets/ArticlesList';
 import articleContent from './article-content';
 import NotFoundPage from "./NotFoundPage";
@@ -10,12 +10,27 @@ const ArticlePage = ({ match }) => {
 
     const otherArticle = articleContent.filter(article => article.name !== name);
 
+    const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+
+    useEffect(async() => {
+        const fetchData = async () => {
+            const result = await fetch(`/api/articles/${name}`);
+            const body = await result.json();
+            setArticleInfo(body);
+        };
+
+        fetchData();
+        
+        setArticleInfo({ upvotes: Math.ceil(Math.random() * 10) })
+    }, [name])
+
     if (!article) return <NotFoundPage></NotFoundPage>
 
     return (
         <>
             <div>
                 <h1>{article.title}</h1>
+                <p> This post has been upvotes {articleInfo.upvotes} times </p>
                 {article.content.map((paragraph, key) => (
                     <p key={key}>{paragraph}</p>
                 ))}
